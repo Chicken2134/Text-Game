8aaf58acc70aca17b3675672fdac1805a5e56b4a
Chicken2134 <123844677+Chicken2134@users.noreply.github.com>
2023-01-29 15:24:28 +0000

    Need fixing

--- Main.py
+++ Main.py
@@ -1,47 +1,10 @@
-<<<<<<< HEAD:Untitled-1.py
+Go = 1
 print("Welcome daring explorers, to the outskirts of Kalvian")
-=======
+print("Please type 1 to continue")
+UserInput = ""
+while userinput is not Go:
+    print("Type go to continue.")
+    if Go is 1: 
+        print("Welcome to the adventure, please click 2 to continue")
+        UserInput = ""
 
-from tkinter import LEFT, RIGHT
-
-
-if __name__ == "__main__":
-  while True:
-    Print("Welcome to Exarila, the Empire of the west.")
-Print("You are a simple trader of the Hitla Dynasty.")
-Print(Choose your Name)
-name = input()
-Print(Good Luck +name+)
-IntroScene()
-
-def introScene():
-directions = [LEFT, RIGHT]
-while userInput not in directions:
-    print("Options: left/right/backward/forward")
-    userInput = input()
-    if userInput == "left":
-      showCitadel()
-    elif userInput == "right":
-      showMarket()
-else: 
- Options = [Kill, Leave]     print("Please enter a valid option.")
-def showCitadel():
-print ("Welcome to the Citadel Comrade!" Exclaims a plump short man.)
-while userInput not in Options: 
-print ("Kill, Leave")
-userInput = ""
-if (userInput == Kill)
-Battlescene()
-elif userInput == Leave
-Leave()
-
-def Battlescene():
-print("Stay back!" yells the wounded man in fear.)
-print("Guards, Seize Him!" Yelled the Man for backup)
-print(The guards grab your arms and knock you out.)
-TheDungeon()
-
-Def Leave():
-print(You Leave)
-print(You end your adventure)
->>>>>>> 42b1c1fb9865633640e787ec2456a1a29a6bd1ff:Main.py
